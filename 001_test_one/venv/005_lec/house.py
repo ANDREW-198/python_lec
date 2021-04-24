fa14c291ac2ba035172d4be0bb100b1b03388ced
Dimka-1985 <>
2021-04-24 04:02:56 +0300

    создал файл с функцие рисования дома.

---
+++ 001_test_one/venv/005_lec/house.py
@@ -0,0 +1,19 @@
+
+x, y = 100, 100
+width, height = 200, 200
+def draw_house(x, y, width, height):
+    """
+    Функция, которая рисует домик полной ширины width и высоты height от опорной точки (x,y),
+    которая находится в середине нижней точки фундамента
+    :param x: координата x середины домика
+    :param y: координата Y низа фундамента
+    :param width: полная ширина домика (фундамент или вылет крыши включены)
+    :param height: полная высота домика
+    :return: None
+    """
+    print("Типа рисую домик...",x, y, width, height)
+    pass #do nothing
+
+draw_house(x, y, width, height)
+
+
